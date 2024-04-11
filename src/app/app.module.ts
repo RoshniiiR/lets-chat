1b42b3ac36eb8e0c0045ba9395c882a2e929d9f3
RoshniiiR <roshni.canopus@gmail.com>
2024-04-11 16:26:32 +0530

    initial-commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import { environment } from '../environment/environment';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+  constructor() {
+    firebase.initializeApp(environment.firebaseConfig);
+  }
+}
---
+++ src/environment/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyC-pcCdL6EYXJKsEW7OqaF1AybJIyDV7l0',
+    authDomain: 'lets-chat-d2936.firebaseapp.com',
+    projectId: 'lets-chat-d2936',
+    storageBucket: 'lets-chat-d2936.appspot.com',
+    messagingSenderId: '781224036793',
+    appId: '1:781224036793:web:f2655e2d90fb1f8de84e68',
+    measurementId: 'G-ECLBH7D0D9',
+  },
+};
